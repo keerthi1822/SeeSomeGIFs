6a670907a47624acb0175ca102152a6be0c14232
keerthi <keerthi1822@gmail.com>
2019-07-18 00:05:23 +0200

    gif

---
+++ gif.js
@@ -0,0 +1,28 @@
+let inputText = document.querySelector('#searchText');
+
+let searchButton = document.querySelector('#searchGif');
+let noText = document.querySelector('#noText');
+let examples = document.querySelector('p');
+
+let img = document.createElement('img');
+img.id = 'gifImg';
+document.body.appendChild(img);
+
+searchButton.addEventListener('click', () => {
+  let searchWord = inputText.value;
+  if (searchWord !== '') {
+    console.log(searchWord);
+    fetch(
+      'https://api.giphy.com/v1/gifs/search?q=' +
+        searchWord +
+        '&api_key=HCRI89ixujLx2ZrfrZEmkrItQzVetl68&limit=5'
+    )
+      .then(response => response.json())
+      .then(gif => {
+        img.src = gif.data[1].images.preview_webp.url;
+        examples.innerHTML = '';
+      });
+  } else {
+    noText.innerHTML = 'please enter text';
+  }
+});
